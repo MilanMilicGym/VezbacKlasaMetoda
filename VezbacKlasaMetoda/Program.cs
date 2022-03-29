a767c6c8054ea971e2656f86f25b872f9f639969
MilanMilicGym <milanmilicgym@gmail.com>
2022-03-29 13:18:21 +0200

    Initial commit

---
+++ VezbacKlasaMetoda/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VezbacKlasaMetoda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Milan = new Vezbac ();
+            Milan.ImeVezbaca = "Milan";
+            Milan.Visina = 183;
+            Milan.Tezina = 88 ;
+            Milan.Godina = 28;
+            Console.WriteLine (Milan.ImeVezbaca + ", njegova visina je  " + Milan.Visina + ", njegova tezina je  " + Milan.Tezina + ", ima godina " + Milan.Godina);
+
+            Milan.Trcanje();
+           var Zoran = new Vezbac ();
+            Zoran.ImeVezbaca = "Zoran";
+            Zoran.DizanjeTegova();
+
+            var Mica = new Vezbac ();
+            Mica.ImeVezbaca = "Mica";
+
+            Mica.PenjanjeNaRipstol();
+        }
+    }
+}
---
+++ VezbacKlasaMetoda/Vezbac.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VezbacKlasaMetoda
+{
+    internal class Vezbac
+    {
+        public string ImeVezbaca { get; set; }
+        public int Visina { get; set; }
+        public int Tezina { get; set; }
+        public int Godina { get; set; }
+
+        public void Trcanje()
+        {
+            Console.WriteLine(ImeVezbaca + " je trcao");
+
+
+        }
+
+        public void DizanjeTegova ()
+        {
+            Console.WriteLine(ImeVezbaca + " je dizao tegove");
+        }
+
+        public void PenjanjeNaRipstol ()
+        {
+            Console.WriteLine(ImeVezbaca + " se popeo na ripstol");
+        }
+
+
+    }
+}
